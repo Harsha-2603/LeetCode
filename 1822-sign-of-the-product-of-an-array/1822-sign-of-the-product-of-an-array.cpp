90d86027ebc6aebdcf8e89ebbbe8c0e8c6a0030c
Harsha-2603 <harshavardhan.thatavarthi@gmail.com>
2025-12-07 23:55:33 +0530

    Time: 0 ms (100%), Space: 13.7 MB (48.34%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -4,8 +4,8 @@ public:
         int sign = 1;
 
         for (int n : nums) {
-            if (n == 0) return 0;   // product becomes zero
-            if (n < 0) sign = -sign; // flip sign for negative number
+            if (n == 0) return 0;        // product becomes zero
+            if (n < 0) sign = -sign;     // flip sign for negative number
         }
 
         return sign;
