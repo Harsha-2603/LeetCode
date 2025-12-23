a1531ef247715b9c6cd9673c27c9c2dd48e3cb2f
Harsha-2603 <harshavardhan.thatavarthi@gmail.com>
2025-12-23 22:08:57 +0530

    Time: 0 ms (100%), Space: 9.9 MB (88.72%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -8,20 +8,48 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* middleNode(ListNode* head) {
+//         ListNode* ptr=head;
+//         int count=0;
+//         while(ptr!=nullptr){
+//             ptr=ptr->next;
+//             count++;
+//         }
+//         count=count/2;
+//         while(count>0){
+//             head=head->next;
+//             count--;
+//         }
+//         return(head);
+//     }
+// };
+
+//optimal using slow and fast poiter approch
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode* ptr=head;
-        int count=0;
-        while(ptr!=nullptr){
-            ptr=ptr->next;
-            count++;
+        if(head==nullptr){
+            return(head);
         }
-        count=count/2;
-        while(count>0){
-            head=head->next;
-            count--;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
         }
+        head=slow;
         return(head);
     }
 };
