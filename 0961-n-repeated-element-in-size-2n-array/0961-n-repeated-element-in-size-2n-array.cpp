3b84c25eb05f77c7d67195e1e0c6e081c98efaca
Harsha-2603 <harshavardhan.thatavarthi@gmail.com>
2026-01-02 15:36:19 +0530

    Time: 27 ms (8.36%), Space: 35.2 MB (6.44%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -3,8 +3,8 @@ public:
     int repeatedNTimes(vector<int>& nums) {
         map<int,int>mp;
         int len=nums.size();
-        for(auto x: nums){
-            mp[x]++;
+        for(auto val: nums){
+            mp[val]++;
         }
         for(auto x : mp){
             if(x.second>1){
