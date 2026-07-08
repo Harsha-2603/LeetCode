95513cc8d0b4b4b54fc98de13cd4ffa898636594
THATAVARTHI HARSHA VARDHAN <harshavardhan.thatavarthi@gmail.com>
2026-07-08 11:09:23 +0530

    Time: 0 ms (100%), Space: 17.6 MB (53.98%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array-optimal-07-08-2026-11-09-15.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int last(vector<int>& nums,int target){
+        int low=0;
+        int high=nums.size()-1;
+        int ans=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]<=target){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        if(ans == -1) return -1;
+        return(ans);
+    }
+    int first(vector<int>& nums,int target){
+        int low=0;
+        int high=nums.size()-1;
+        int ans=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]>=target){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        if(ans == -1) return -1;
+        return(ans);
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<int> vec;
+        int a=first(nums,target);
+        int b=last(nums,target);
+        if (a == -1 || nums[a] != target)
+            return {-1, -1};
+        else if (b == -1 || nums[b] != target)
+            return {-1, -1};    
+        return {a, b};
+    }
+};
