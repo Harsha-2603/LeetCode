50807474541b616e0893d961e82e5388d89500a1
THATAVARTHI HARSHA VARDHAN <harshavardhan.thatavarthi@gmail.com>
2026-07-25 09:27:01 +0530

    Time: 0 ms (100%), Space: 15.7 MB (75.42%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation-07-25-2026-09-26-50.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        int index=-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                index=i;
+                break;
+            }
+        }
+        if(index==-1){
+            reverse(nums.begin(),nums.end());
+        }
+        else if(index>=0){
+            int min=INT_MAX;
+            for(int j=n-1;j>index;j--){
+                if(nums[j]>nums[index]){
+                    swap(nums[j],nums[index]);
+                    break;
+                }
+            }
+            sort(nums.begin()+index+1,nums.end());
+        }
+        
+    }
+};
