86dac3430f09a10cb2c9eecebb02790ce96cc707
THATAVARTHI HARSHA VARDHAN <harshavardhan.thatavarthi@gmail.com>
2026-08-21 21:47:05 +0530

    Time: 0 ms (100%), Space: 7.8 MB (93.59%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number-08-21-2026-21-47-04.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int count=0;
+        int res=start^goal;
+        while(res>0){
+            if(res&1){
+                count++;
+            }
+            res=res>>1;
+        }
+        return(count);
+    }
+};
