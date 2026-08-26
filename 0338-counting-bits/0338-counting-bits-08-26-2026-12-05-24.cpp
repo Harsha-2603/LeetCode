c21d92ea2b6e9cc4432a9a6be90175cb9cca24d8
THATAVARTHI HARSHA VARDHAN <harshavardhan.thatavarthi@gmail.com>
2026-08-26 12:05:27 +0530

    Time: 0 ms (100%), Space: 11.3 MB (28.73%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits-08-26-2026-12-05-24.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> arr;
+        for(int i=0;i<=n;i++){
+            int count=0;
+            int n=i;
+            while(n!=0){
+                n=n&(n-1);
+                count++;
+            }
+            arr.push_back(count);
+        }
+        return(arr);
+    }
+};
