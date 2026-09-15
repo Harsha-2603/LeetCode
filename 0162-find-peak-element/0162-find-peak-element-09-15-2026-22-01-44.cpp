d08796cd3a503ec9e811bb542e2715fc9d801584
THATAVARTHI HARSHA VARDHAN <harshavardhan.thatavarthi@gmail.com>
2026-09-15 22:01:49 +0530

    Time: 0 ms (100%), Space: 12.7 MB (15.34%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element-09-15-2026-22-01-44.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n=nums.size();
+        int index=0;
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(mid+1<n && nums[mid]<nums[mid+1]){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return(low);
+    }
+};
